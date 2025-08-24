aa758e7c2263c1e10b7880473502c98072109a65
aasishdarrel <aasishdairel@gmail.com>
2025-08-24 13:23:45 +0530

    type conversion(explicitly mensioned)

---
+++ typeconversion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main () {
+
+
+    long long gg= 1234567890123456789ll;
+    std::cout<<static_cast<int>(gg)<<std::endl;
+
+    double Pi = 3.14;
+
+    std::cout<<static_cast<int>(Pi)<<std::endl;
+}
