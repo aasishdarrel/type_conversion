be25ac74e0ad13d43036e7087fbc39caffb2b912
aasishdarrel <aasishdairel@gmail.com>
2025-08-24 11:52:11 +0530

    type conversion

---
+++ type_conversion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+auto a = 'A';
+
+auto b = 34;
+int main() {
+    std::cout<<a+b<<std::endl;
+return 0;
+}
+// implict type conversion
+
+/* stored is a ASCII val and b is stored in a integer value and the type conversion takes place automatically */
